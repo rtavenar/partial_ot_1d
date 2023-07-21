37ee77ac0a9402355a05bbd95830c8e574cbe5b7
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2023-07-21 14:59:20 +0200

    Add std info

--- n_groups.py
+++ n_groups.py
@@ -9,18 +9,20 @@ n_repeat = 10
 n = 1000
 
 steps = list(range(1, n + 1))
-n_groups = np.zeros((n, ), dtype=int)
-for _ in range(n_repeat):
+n_groups = np.zeros((n_repeat, n), dtype=int)
+for i in range(n_repeat):
     x = np.random.rand(n)
     y = np.random.rand(n)
 
     solutions = partial_ot_1d(x, y)
-    n_groups += [len(groups) for groups in solutions]
+    n_groups[i] = [len(groups) for groups in solutions]
     
 plt.figure()
-plt.plot(steps, n_groups / n_repeat)
+plt.plot(steps, np.mean(n_groups, axis=0), linestyle="solid", color="tab:blue")
+plt.plot(steps, np.mean(n_groups, axis=0) + np.std(n_groups, axis=0), linestyle="dashed", color="tab:blue")
+plt.plot(steps, np.mean(n_groups, axis=0) - np.std(n_groups, axis=0), linestyle="dashed", color="tab:blue")
 plt.xlabel("Step")
-plt.ylabel("Average number of groups")
+plt.ylabel("Average number of groups (+/- standard deviation)")
 plt.tight_layout()
 plt.grid()
 plt.show()

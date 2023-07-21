c14769e12433de1feae4cbe37df9bb305e77cc5d
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2023-07-21 14:49:56 +0200

    Add test on evolution of number of groups

---
+++ n_groups.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from partial import partial_ot_1d
+
+np.random.seed(0)
+n_repeat = 10
+
+n = 1000
+
+steps = list(range(1, n + 1))
+n_groups = np.zeros((n, ), dtype=int)
+for _ in range(n_repeat):
+    x = np.random.rand(n)
+    y = np.random.rand(n)
+
+    solutions = partial_ot_1d(x, y)
+    n_groups += [len(groups) for groups in solutions]
+    
+plt.figure()
+plt.plot(steps, n_groups)
+plt.xlabel("Step")
+plt.ylabel("Average number of groups")
+plt.tight_layout()
+plt.grid()
+plt.show()

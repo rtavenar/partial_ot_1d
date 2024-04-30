4ddcaad2670a80626b3557ab2395f9399c51ccba
Laetitia Chapel <laetitia.chapel@univ-ubs.fr>
2024-04-30 11:26:51 +0200

    add naive implementation with groups and series - l1 distance

---
+++ crapy implem with series/sanity_check.py
@@ -0,0 +1,27 @@
+﻿import ot
+import numpy as np 
+from partialOT_crapy import partial_ot
+
+np.random.seed(0)
+
+n = 6
+x = np.random.rand(n)
+y = np.random.rand(n)
+
+x_s = np.sort(x)
+y_s = np.sort(y)
+M = ot.dist(x_s.reshape(-1, 1), y_s.reshape(-1, 1), metric="minkowski", p=1)
+current_series_idx, current_series_cost, active_x, active_y, active_cost = partial_ot(x_s, y_s)
+for i in np.arange(1,len(x)+1):
+    print("--------i", i)
+    mass = i/ len(x)  
+    print("diff", ot.partial.partial_wasserstein2([], [], M, mass) - active_cost[i-1])
+    if (np.abs(ot.partial.partial_wasserstein2([], [], M, mass) - active_cost[i-1]) > 1e-8):
+        print(ot.partial.partial_wasserstein2([], [], M, mass),active_cost[i-1] )
+        print(active_x)
+        print(active_y)
+        print("current series and costs")
+        print(current_series_idx)
+        print(current_series_cost)
+        t = ot.partial.partial_wasserstein([], [], M, mass)
+        print(np.where(t > 1e-6))
---
+++ crapy implem with series/timings_crapy.py
@@ -0,0 +1,36 @@
+﻿import time
+import numpy as np
+import matplotlib.pyplot as plt
+from partialOT_crapy import partial_ot, init_useful_vectors, compute_costs_for_reg_path
+
+#from scalable_partial import PartialOT1d
+
+np.random.seed(0)
+n_repeat = 10
+
+values_for_n =  [10, 30, 100, 300, 1000, 3000, 10000]
+timings = []
+
+
+
+
+
+for n in values_for_n:
+    x = list(np.random.rand(n))
+    y = list(np.random.rand(n))
+
+    t0 = time.time()
+    for _ in range(n_repeat):
+        #PartialOT1d(max_iter=n).fit(x, y)
+        partial_ot(x,y)
+    avg_time = (time.time() - t0) / n_repeat
+    timings.append(avg_time)
+    print(f"n={n:04d}\ttime={avg_time}\tvalue of constant C in complexity (C * n log n)={avg_time / (n * np.log(n))}")
+    
+plt.figure()
+plt.loglog(values_for_n, timings)
+plt.xlabel("Number of points in x (resp. y)")
+plt.ylabel("Average time (in seconds) for the resolution of a partial problem")
+plt.tight_layout()
+plt.grid()
+plt.show()

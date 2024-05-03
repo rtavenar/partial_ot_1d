f2417b257ba99dbc99b2666aaa105303b4cd0127
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2024-05-03 17:59:20 +0200

    minor

--- timings_partial.py
+++ timings_partial.py
@@ -15,13 +15,14 @@ values_for_n =  [1000, 3000, 10000, 30000, 100000, 300000, 1000000]
 timings_ours = []
 timings_aistats22 = []
 lambdas_cvpr23 = [10]  # [1, 10, 100]
+rho_aistats22 = 1.
 timings_cvpr23 = {k: [] for k in lambdas_cvpr23}
 
 # Cache stuff for competitors
 CVPR23Bai_partial_solver(np.sort(np.random.rand(10)), np.sort(np.random.rand(10)), 10)
 AISTATS22Sejourne_unbalanced_solver(np.ones(10), np.ones(10),
                                     np.sort(np.random.rand(10)), np.sort(np.random.rand(10)), 
-                                    p=1, rho1=1., rho2=1.)
+                                    p=1, rho1=rho_aistats22, rho2=rho_aistats22)
 
 for n in values_for_n:
     sum_time_ours = 0.
@@ -47,7 +48,7 @@ for n in values_for_n:
         AISTATS22Sejourne_unbalanced_solver(
             np.ones(n), np.ones(n),
             np.sort(x), np.sort(y), 
-            p=1, rho1=1., rho2=1.
+            p=1, rho1=rho_aistats22, rho2=rho_aistats22
         )
         sum_time_aistats22 += (time.time() - t0)
     timings_ours.append(sum_time_ours / n_repeat)
@@ -61,7 +62,7 @@ plt.figure(figsize=formatter.line_width_plot(aspect_ratio="normal"))
 plt.loglog(values_for_n, timings_ours, label="Our method")
 for l in lambdas_cvpr23:
     plt.loglog(values_for_n, timings_cvpr23[l], label=f"OPT, $\lambda={l}$")  # (Bai et al, CVPR 2023)")
-plt.loglog(values_for_n, timings_aistats22, label=f"Fast-UOT, $\\rho_1 = \\rho_2 = 1$")  # (Séjourné et al, AISTATS 2022)")
+plt.loglog(values_for_n, timings_aistats22, label=f"Fast-UOT, $\\rho_1 = \\rho_2 = {rho_aistats22}$")  # (Séjourné et al, AISTATS 2022)")
 plt.xlabel("Number of points in x (resp. y)")
 plt.ylabel("Average time (in seconds)")
 plt.tight_layout()

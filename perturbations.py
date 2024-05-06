ff4874160a21b72ad3aa1571aa4c38d6e46ba6b9
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2024-05-06 09:18:19 +0200

    perturbed in torch

--- perturbations.py
+++ perturbations.py
@@ -1,5 +1,9 @@
 # coding=utf-8
-# Copyright 2024 The Google Research Authors.
+# 
+# Modifications from original work
+# 29-03-2021 (tuero@ualberta.ca) : Convert Tensorflow code to PyTorch
+#
+# Copyright 2021 The Google Research Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,15 +17,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Lint as: python3
 """Introduces differentiation via perturbations.
 
 Example of usage:
 
   @perturbed
   def sign_or(x, axis=-1):
-    s = tf.cast((tf.sign(x) + 1)  / 2.0, dtype=tf.bool)
-    result = tf.math.reduce_any(s, axis=axis)
-    return tf.cast(result, dtype=x.dtype) * 2.0 - 1.0
+    s = ((torch.sign(x) + 1) / 2.0).type(torch.bool)
+    result = torch.any(s, dim=-1)
+    return result.type(torch.float) * 2.0 - 1
 
 
 Then sign_or is differentiable (unlike what it seems).
@@ -31,154 +36,168 @@ It is possible to specify the parameters of the perturbations using:
   ...
 
 The decorator can also be used directly as a function, for example:
-  soft_argsort = perturbed(tf.argsort, num_samples=200, sigma=0.01)
+  soft_argsort = perturbed(torch.argsort, num_samples=200, sigma=0.01)
 """
 
 import functools
 from typing import Tuple
-import tensorflow.compat.v2 as tf
-import tensorflow_probability as tfp
+import torch
+from torch.distributions.gumbel import Gumbel
+from torch.distributions.normal import Normal
 
 _GUMBEL = 'gumbel'
 _NORMAL = 'normal'
 SUPPORTED_NOISES = (_GUMBEL, _NORMAL)
 
 
-def sample_noise_with_gradients(
-    noise, shape):
-  """Samples a noise tensor according to a distribution with its gradient.
+def sample_noise_with_gradients(noise, shape):
+    """Samples a noise tensor according to a distribution with its gradient.
 
-  Args:
-   noise: (str) a type of supported noise distribution.
-   shape: tf.Tensor<int>, the shape of the tensor to sample.
+    Args:
+    noise: (str) a type of supported noise distribution.
+    shape: torch.tensor<int>, the shape of the tensor to sample.
 
-  Returns:
-   A tuple Tensor<float>[shape], Tensor<float>[shape] that corresponds to the
-   sampled noise and the gradient of log the underlying probability
-   distribution function. For instance, for a gaussian noise (normal), the
-   gradient is equal to the noise itself.
+    Returns:
+    A tuple Tensor<float>[shape], Tensor<float>[shape] that corresponds to the
+    sampled noise and the gradient of log the underlying probability
+    distribution function. For instance, for a gaussian noise (normal), the
+    gradient is equal to the noise itself.
 
-  Raises:
-   ValueError in case the requested noise distribution is not supported.
-   See perturbations.SUPPORTED_NOISES for the list of supported distributions.
-  """
-  if noise not in SUPPORTED_NOISES:
-    raise ValueError('{} noise is not supported. Use one of [{}]'.format(
-        noise, SUPPORTED_NOISES))
+    Raises:
+    ValueError in case the requested noise distribution is not supported.
+    See perturbations.SUPPORTED_NOISES for the list of supported distributions.
+    """
+    if noise not in SUPPORTED_NOISES:
+        raise ValueError('{} noise is not supported. Use one of [{}]'.format(
+            noise, SUPPORTED_NOISES))
 
-  if noise == _GUMBEL:
-    sampler = tfp.distributions.Gumbel(0.0, 1.0)
-    samples = sampler.sample(shape)
-    gradients = 1 - tf.math.exp(-samples)
-  elif noise == _NORMAL:
-    sampler = tfp.distributions.Normal(0.0, 1.0)
-    samples = sampler.sample(shape)
-    gradients = samples
+    if noise == _GUMBEL:
+        sampler = Gumbel(0.0, 1.0)
+        samples = sampler.sample(shape)
+        gradients = 1 - torch.exp(-samples)
+    elif noise == _NORMAL:
+        sampler = Normal(0.0, 1.0)
+        samples = sampler.sample(shape)
+        gradients = samples
 
-  return samples, gradients
+    return samples, gradients
 
 
 def perturbed(func=None,
               num_samples = 1000,
               sigma = 0.05,
               noise = _NORMAL,
-              batched = True):
-  """Turns a function into a differentiable one via perturbations.
+              batched = True,
+              device=None):
+    """Turns a function into a differentiable one via perturbations.
 
-  The input function has to be the solution to a linear program for the trick
-  to work. For instance the maximum function, the logical operators or the ranks
-  can be expressed as solutions to some linear programs on some polytopes.
-  If this condition is violated though, the result would not hold and there is
-  no guarantee on the validity of the obtained gradients.
+    The input function has to be the solution to a linear program for the trick
+    to work. For instance the maximum function, the logical operators or the ranks
+    can be expressed as solutions to some linear programs on some polytopes.
+    If this condition is violated though, the result would not hold and there is
+    no guarantee on the validity of the obtained gradients.
 
-  This function can be used directly or as a decorator.
+    This function can be used directly or as a decorator.
 
-  Args:
-   func: the function to be turned into a perturbed and differentiable one.
+    Args:
+    func: the function to be turned into a perturbed and differentiable one.
     Four I/O signatures for func are currently supported:
-     If batched is True,
-      (1) input [B, D1, ..., Dk], output [B, D1, ..., Dk], k >= 1
-      (2) input [B, D1, ..., Dk], output [B], k >= 1
-     If batched is False,
-      (3) input [D1, ..., Dk], output [D1, ..., Dk], k >= 1
-      (4) input [D1, ..., Dk], output [], k >= 1.
-   num_samples: the number of samples to use for the expectation computation.
-   sigma: the scale of the perturbation.
-   noise: a string representing the noise distribution to be used to sample
+        If batched is True,
+        (1) input [B, D1, ..., Dk], output [B, D1, ..., Dk], k >= 1
+        (2) input [B, D1, ..., Dk], output [B], k >= 1
+        If batched is False,
+        (3) input [D1, ..., Dk], output [D1, ..., Dk], k >= 1
+        (4) input [D1, ..., Dk], output [], k >= 1.
+    num_samples: the number of samples to use for the expectation computation.
+    sigma: the scale of the perturbation.
+    noise: a string representing the noise distribution to be used to sample
     perturbations.
-   batched: whether inputs to the perturbed function will have a leading batch
+    batched: whether inputs to the perturbed function will have a leading batch
     dimension (True) or consist of a single example (False). Defaults to True.
+    device: The device to create tensors on (cpu/gpu). If None given, it will
+    default to gpu:0 if available, cpu otherwise.
 
-  Returns:
-   a function has the same signature as func but that can be back propagated.
-  """
-  # This is a trick to have the decorator work both with and without arguments.
-  if func is None:
-    return functools.partial(
-        perturbed, num_samples=num_samples, sigma=sigma, noise=noise,
-        batched=batched)
+    Returns:
+    a function has the same signature as func but that can be back propagated.
+    """
+    # If device not supplied, auto detect
+    if device is None:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # This is a trick to have the decorator work both with and without arguments.
+    if func is None:
+        return functools.partial(
+            perturbed, num_samples=num_samples, sigma=sigma, noise=noise,
+            batched=batched, device=device)
 
-  @functools.wraps(func)
-  def wrapper(input_tensor, *args, **kwargs):
-    @tf.custom_gradient
-    def forward(input_tensor, *args, **kwargs):
-      """The differentiation by perturbation core routine."""
-      original_input_shape = tf.shape(input_tensor)
-      if batched:
-        tf.debugging.assert_rank_at_least(
-            input_tensor, 2, 'Batched inputs must have at least rank two')
-      else:  # Adds dummy batch dimension internally.
-        input_tensor = tf.expand_dims(input_tensor, 0)
-      input_shape = tf.shape(input_tensor)  # [B, D1, ... Dk], k >= 1
-      perturbed_input_shape = tf.concat([[num_samples], input_shape], axis=0)
+    @functools.wraps(func)
+    def wrapper(input_tensor, *args):
+        class PerturbedFunc(torch.autograd.Function):
 
-      noises = sample_noise_with_gradients(noise, perturbed_input_shape)
-      additive_noise, noise_gradient = tuple(
-          [tf.cast(noise, dtype=input_tensor.dtype) for noise in noises])
-      perturbed_input = tf.expand_dims(input_tensor, 0) + sigma * additive_noise
+            @staticmethod
+            def forward(ctx, input_tensor, *args):
+                original_input_shape = input_tensor.shape
+                if batched:
+                    if not input_tensor.dim() >= 2:
+                        raise ValueError('Batched inputs must have at least rank two')
+                else:  # Adds dummy batch dimension internally.
+                    input_tensor = input_tensor.unsqueeze(0)
+                input_shape = input_tensor.shape  # [B, D1, ... Dk], k >= 1
+                perturbed_input_shape = [num_samples] + list(input_shape)
 
-      # [N, B, D1, ..., Dk] -> [NB, D1, ..., Dk].
-      flat_batch_dim_shape = tf.concat([[-1], input_shape[1:]], axis=0)
-      perturbed_input = tf.reshape(perturbed_input, flat_batch_dim_shape)
-      # Calls user-defined function in a perturbation agnostic manner.
-      perturbed_output = func(perturbed_input, *args, **kwargs)
-      # [NB, D1, ..., Dk] ->  [N, B, D1, ..., Dk].
-      perturbed_input = tf.reshape(perturbed_input, perturbed_input_shape)
-      # Either
-      #   (Default case): [NB, D1, ..., Dk] ->  [N, B, D1, ..., Dk]
-      # or
-      #   (Full-reduce case) [NB] -> [N, B]
-      perturbed_output_shape = tf.concat(
-          [[num_samples], [-1], tf.shape(perturbed_output)[1:]], axis=0)
-      perturbed_output = tf.reshape(perturbed_output, perturbed_output_shape)
+                noises = sample_noise_with_gradients(noise, perturbed_input_shape)
+                additive_noise, noise_gradient = tuple(
+                    [noise.type(input_tensor.dtype) for noise in noises])
+                additive_noise = additive_noise.to(device)
+                noise_gradient = noise_gradient.to(device)
+                perturbed_input = input_tensor.unsqueeze(0) + sigma * additive_noise
 
-      forward_output = tf.reduce_mean(perturbed_output, axis=0)
-      if not batched:  # Removes dummy batch dimension.
-        forward_output = forward_output[0]
+                # [N, B, D1, ..., Dk] -> [NB, D1, ..., Dk].
+                flat_batch_dim_shape = [-1] + list(input_shape)[1:]
+                perturbed_input = torch.reshape(perturbed_input, flat_batch_dim_shape)
+                # Calls user-defined function in a perturbation agnostic manner.
+                perturbed_output = func(perturbed_input, *args)
+                # [NB, D1, ..., Dk] ->  [N, B, D1, ..., Dk].
+                perturbed_input = torch.reshape(perturbed_input, perturbed_input_shape)
+                # Either
+                #   (Default case): [NB, D1, ..., Dk] ->  [N, B, D1, ..., Dk]
+                # or
+                #   (Full-reduce case) [NB] -> [N, B]
+                perturbed_output_shape = [num_samples, -1] + list(perturbed_output.shape)[1:]
+                perturbed_output = torch.reshape(perturbed_output, perturbed_output_shape)
 
-      def grad(dy):
-        """Compute the gradient of the expectation via integration by parts."""
-        output, noise_grad = perturbed_output, noise_gradient
-        # Adds dummy feature/channel dimension internally.
-        if perturbed_input.shape.rank > output.shape.rank:
-          dy = tf.expand_dims(dy, axis=-1)
-          output = tf.expand_dims(output, axis=-1)
-        # Adds dummy batch dimension internally.
-        if not batched:
-          dy = tf.expand_dims(dy, axis=0)
-        # Flattens [D1, ..., Dk] to a single feat dim [D].
-        flatten = lambda t: tf.reshape(t, (tf.shape(t)[0], tf.shape(t)[1], -1))
-        dy = tf.reshape(dy, (tf.shape(dy)[0], -1))  # (B, D)
-        output = flatten(output)  # (N, B, D)
-        noise_grad = flatten(noise_grad)  # (N, B, D)
+                forward_output = torch.mean(perturbed_output, dim=0)
+                if not batched:  # Removes dummy batch dimension.
+                    forward_output = forward_output[0]
 
-        g = tf.einsum('nbd,nb->bd', noise_grad,
-                      tf.einsum('nbd,bd->nb', output, dy))
-        g /= sigma * num_samples
-        return tf.reshape(g, original_input_shape)
+                # Save context for backward pass
+                ctx.save_for_backward(perturbed_input, perturbed_output, noise_gradient)
+                ctx.original_input_shape = original_input_shape
 
-      return forward_output, grad
+                return forward_output
 
-    return forward(input_tensor, *args, **kwargs)
+            @staticmethod
+            def backward(ctx, dy):
+                # Pull saved tensors
+                original_input_shape = ctx.original_input_shape
+                perturbed_input, perturbed_output, noise_gradient = ctx.saved_tensors
+                output, noise_grad = perturbed_output, noise_gradient
+                # Adds dummy feature/channel dimension internally.
+                if perturbed_input.dim() > output.dim():
+                    dy = dy.unsqueeze(-1)
+                    output = output.unsqueeze(-1)
+                # Adds dummy batch dimension internally.
+                if not batched:
+                    dy = dy.unsqueeze(0)
+                # Flattens [D1, ..., Dk] to a single feat dim [D].
+                flatten = lambda t: torch.reshape(t, (list(t.shape)[0], list(t.shape)[1], -1))
+                dy = torch.reshape(dy, (list(dy.shape)[0], -1))  # (B, D)
+                output = flatten(output)  # (N, B, D)
+                noise_grad = flatten(noise_grad)  # (N, B, D)
 
-  return wrapper
+                g = torch.einsum('nbd,nb->bd', noise_grad, torch.einsum('nbd,bd->nb', output, dy))
+                g /= sigma * num_samples
+                return torch.reshape(g, original_input_shape)
+
+        return PerturbedFunc.apply(input_tensor, *args)
+
+    return wrapper
--- perturbed_sliced.py
+++ perturbed_sliced.py
@@ -1,19 +1,23 @@
 import numpy as np
-import tensorflow as tf
-import perturbations
+import torch
+import perturbations as perturbations
 
 from sliced import SlicedPartialOT, PartialOT1d
 
 class PerturbedMaheySlicedPartialOT(SlicedPartialOT):
-    def __init__(self, max_iter_gradient, max_iter_partial=None) -> None:
+    def __init__(self, max_iter_gradient, max_iter_partial=None, device=None) -> None:
         self.max_iter_gradient = max_iter_gradient
         self.max_iter_partial = max_iter_partial
         self.partial_problem = PartialOT1d(self.max_iter_partial)
+        if device is not None:
+            self.device = device
+        else:
+            self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     def project_in_1d(self, x, y, w):
-        w /= np.sqrt(np.sum(w ** 2, axis=-1, keepdims=True))
-        proj_x = np.dot(w, x.T).T
-        proj_y = np.dot(w, y.T).T
+        # w /= torch.sqrt(torch.sum(w ** 2, axis=-1, keepdims=True))
+        proj_x = (w @ x.T).T
+        proj_y = (w @ y.T).T
         return proj_x, proj_y
 
     def fit(self, x, y):
@@ -35,30 +39,30 @@ class PerturbedMaheySlicedPartialOT(SlicedPartialOT):
                 cost = np.sum(np.abs(subset_x - subset_y))
                 perturbed_costs.append(cost)
 
-            return tf.cast(tf.stack(perturbed_costs), dtype=tf.float32)
+            return torch.tensor(perturbed_costs)
         
         pert_action = perturbations.perturbed(action,
                                               num_samples=1000,
                                               sigma=0.5,
                                               noise='gumbel',
-                                              batched=False)
+                                              batched=False, 
+                                              device=self.device)
 
         min_cost = np.inf
         bool_indices_x, bool_indices_y = None, None
-        w = tf.Variable(self.draw_direction(d), dtype=tf.float32)
+        w = torch.tensor(self.draw_direction(d), requires_grad=True)
+        opt = torch.optim.SGD([w], lr=0.01, momentum=0.9)
         for _ in range(self.max_iter_gradient):
-            with tf.GradientTape() as tape:
-                theta = tf.Variable(w)
-                pert_cost = pert_action(theta)
-                grad_pert, = tape.gradient(pert_cost, [theta])
-            
-            w = tf.Variable(w - eta * grad_pert, dtype=tf.float32)
+            opt.zero_grad()
+            output = pert_action(w)
+            output.backward(torch.ones_like(output))
+            opt.step()
 
-            cost = float(pert_cost)
+            cost = float(output)
             print(cost)
 
             if cost < min_cost:
-                best_w = np.array(w)
+                best_w = w.detach().numpy()
                 min_cost = cost
         
         proj_x, proj_y = self.project_in_1d(x, y, best_w)
@@ -79,15 +83,15 @@ if __name__ == "__main__":
 
     outliers_x = np.random.choice(n, size=n_outliers, replace=False)
     ind_outliers_x = np.zeros((n, d))
-    ind_outliers_x[outliers_x] = 10.
+    ind_outliers_x[outliers_x] = 3.
     x = np.random.rand(n, d) + ind_outliers_x
 
     outliers_y = np.random.choice(n, size=n_outliers, replace=False)
     ind_outliers_y = np.zeros((n, d))
-    ind_outliers_y[outliers_y] = 10.
+    ind_outliers_y[outliers_y] = 3.
     y = np.random.rand(n, d) - ind_outliers_y
     
-    sliced = PerturbedMaheySlicedPartialOT(max_iter_gradient=20, max_iter_partial=n-n_outliers)
+    sliced = PerturbedMaheySlicedPartialOT(max_iter_gradient=5, max_iter_partial=n-n_outliers)
     _, bool_ind_x, bool_ind_y = sliced.fit(x, y)
     print("x")
     print(np.sum(bool_ind_x), np.sum(bool_ind_x[outliers_x]))
